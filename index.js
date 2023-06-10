8cc7f36bbd09e0b3bdddebfec43eeb20fafb0739
SathiyamV <88426005+SathiyamV@users.noreply.github.com>
2023-06-10 18:28:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passOne = document.getElementById("pass1")
+let isAlive = false
+let passwordLength = 12
+
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+function generateRandomPassword() {
+    let randomPassword = ""
+    let isAlive = true
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter()         
+    }
+    return passOne.textContent = randomPassword
+}
+
+function copyText() {
+    let copyGfGText =
+        document.getElementById("pass1");
+
+    copyGfGText.select();
+    document.execCommand("copy");
+}
